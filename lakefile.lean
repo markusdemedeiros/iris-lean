f6c04950221b7a9584c37ef44a20357095a066b8
Markus de Medeiros <markusdemedeiros@outlook.com>
2024-11-16 16:07:58 -0500

    importgraph

--- lakefile.lean
+++ lakefile.lean
@@ -6,6 +6,7 @@ package iris where
 
 require batteries from git "https://github.com/leanprover-community/batteries" @ "main"
 require Qq from git "https://github.com/gebner/quote4" @ "master"
+require "leanprover-community" / "importGraph" @ git "main"
 
 @[default_target]
 lean_lib Iris
