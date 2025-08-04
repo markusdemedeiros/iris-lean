28332b2fe5e14b68eb2e29bd74151df306239627
Markus de Medeiros <markusdemedeiros@outlook.com>
2025-08-04 09:08:26 -0400

    fix

--- src/Iris/Instances/heProp.lean
+++ src/Iris/Instances/heProp.lean
@@ -38,7 +38,7 @@ def heProp_auth (m : H V) : heProp F K V H :=
   UPred.ownM <| ●V HasHHMap.hhmap (fun (_ : K) (v : V) => some (toAgree <| .mk v)) m
 
 def heProp_frag (m : H V) : heProp F K V H :=
-  UPred.ownM <| ◯V StoreO.map (.lift fun v => (.own One.one, toAgree <| .mk v)) ⟨m⟩
+  UPred.ownM <| ◯V HasHHMap.hhmap (fun (_ : K) (v : V) => some (DFrac.own 1, toAgree <| .mk v)) m
 
 def heProp_elem (k : K) (v : V) : heProp F K V H :=
   heProp_frag F K V H (Heap.point k (.some v))
